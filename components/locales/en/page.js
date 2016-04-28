0cc23c751d61fc11daa01e3ff37d81cc505b57d5
Angelo Rogério Rubin <angelorubin@gmail.com>
2016-04-28 15:24:31 -0300

    tradução do arquivo para inglês

--- components/locales/en/page.js
+++ components/locales/en/page.js
@@ -1,8 +1,115 @@
 'use strict'
 
 export default {
-  read_in_locale: 'LEIA EM PORTUGUÊS',
+  read_in_locale: 'READ IN ENGLISH',
   date: '4th and 5th July',
   location: 'São Paulo, Brazil',
-  buy_tickets_now: 'BUY YOUR TICKETS NOW'
+  buy_tickets_now: 'BUY YOUR TICKETS NOW',
+  buy_tickets: 'Buy Tickets!',
+  buy_tickets_v2: 'BUY TICKETS!',
+  code_of_conduct: 'Code of Conduct',
+  sponsorships: 'Sponsors',
+  place: 'Place',
+  schedule: 'Schedule',
+  speakers: 'Speakers',
+  about: 'About',
+  about_the_event: 'About the event',
+  close: 'Close',
+  where_title: 'Will be at Teatro Frei Caneca',
+  open_map: 'OPEN ON GOOGLE MAPS',
+  want_to_know_you: 'We want to know you',
+  want_to_know_you_description: `Fill in the box below with your email and stay on top of everything that happens in this NodeConf Brazil.`,
+  where_description: `The event will take place at Teatro Frei Caneca, easily accessible for all, close to Avenida Paulista and Rua Consolação, 
+  postcards Paulista capital. The location is very wide. We will have lectures in the auditorium and lounge other interesting activities such as workshops 
+  and demo days of Brazilian startups.`,
+  want_to_know_more_about: 'I want to receive information about NodeConf BR 2016',
+  doubts_contact: 'doubts: nodebr@nodebr.org',
+  footnote: `Organizers, sponsors and participants are required to follow the Code of Conduct.`,
+  event_description: `
+    <p>
+      This is the first edition of NodeConf in Latin America and is also the first major event focused exclusively on Node.js Brazil.
+	  We will have an incredible experience in São Paulo in 2 days filled with big speeches and a few surprises waiting for you.
+    </p>
+    <p>
+      In this first edition, we bring news about how is the current scenario of Node.js. 
+      Let's create a contact environment between companies and developers, a space to disseminate knowledge and cool designs made by the community and we will also have special guests that are influence on Node.js worldwide.
+    </p>
+    <p>
+      <a href="http://www.eventick.com.br/nodeconfbr" target="_blank">
+        Compre seu ingresso
+      </a> e garanta uma vaga nesta NodeConf histórica!
+    </p>`,
+  code_of_conduct_description: `
+    <h1>Código de Conduta</h1>
+    <p>
+      Todos os participantes, palestrantes, patrocinadores e voluntários da
+      conferência precisam concordar com este código de conduta. Os
+      organizadores vão reforçar que este código seja seguido durante todo o
+      evento. Esperamos a cooperação de todos os participantes para ajudar a
+      garantir um ambiente seguro para todos.
+    </p>
+    <h2>Precisa de ajuda?</h2>
+    <p>
+      Fale com a NodeBR via
+        <a href="https://www.facebook.com/brNode">Facebook</a>,
+        <a href="https://twitter.com/nodebr">Twitter</a>,
+        <a href="mailto:nodebr@nodebr.org">e-mail</a>
+      ou envie uma conversa privada para meu Telegram
+        <a href="http://telegram.me/nodebr">@nodebr</a>.
+      Caso tenha algum problema sinta-se à vontade para falar, sua privacidade
+      será garantida.
+    </p>
+    <h2>Versão curta</h2>
+    <p>
+      Nossa conferência é dedicada a fornecer uma experiência de conferência
+      livre de assédio para todos, independentemente do sexo, identidade de
+      gênero e expressão, idade, orientação sexual, deficiência, aparência
+      física, tamanho corporal, cor de pele, etnia, religião (ou falta dela) ou
+      escolhas de tecnologias. Nós não toleramos o assédio aos participantes da
+      conferência, sob qualquer forma. Linguagem e imagens sexuais não são
+      apropriadas em nenhum local da conferência, incluindo palestras,
+      workshops, festas, Twitter e outras mídias on-line. Os participantes que
+      violarem estas regras poderão ser punidos ou expulsos da conferência sem
+      restituição, a critério dos organizadores da conferência.
+    </p>
+    <h2>Versão normal</h2>
+    <p>
+      Assédio inclui comentários verbais ofensivos relacionados ao gênero,
+      identidade de gênero e expressão, idade, orientação sexual, deficiência,
+      aparência física, tamanho corporal, cor de pele, etnia, religião, escolhas
+      de tecnologias, imagens sexuais em espaços públicos, intimidação
+      deliberada, perseguição, stalking, fotografias ou filmagens
+      constrangedoras, interrupção contínua das apresentações ou outros
+      eventos, contato físico inadequado e atenção sexual indesejada. Os
+      participantes que receberem uma solicitação para interromper qualquer
+      comportamento de assédio devem fazê-lo imediatamente. Os patrocinadores
+      também estão sujeitos às políticas antiassédio. Particularmente, os
+      patrocinadores não devem usar imagens, atividades ou outro material de
+      cunho sexual. As equipes de estandes e tendas (incluindo voluntários) não
+      devem utilizar roupas, uniformes ou trajes sexualizados ou de outra forma
+      criar um ambiente sexualizado. Se um participante se envolve em
+      comportamento de assédio, os organizadores da conferência podem tomar
+      todas as medidas que considerem adequadas, incluindo avisar o ofensor ou
+      expulsá-lo da conferência sem nenhum reembolso. Caso seja assediado,
+      perceba que alguém está sendo assediado ou tenha alguma dúvida, entre em
+      contato com um membro da equipe da conferência imediatamente. Membros da
+      equipe da conferência podem ser identificados pois estarão vestindo
+      camisetas marcadas. A equipe da conferência estará disposta a auxiliar os
+      participantes a contatarem a segurança do hotel ou local, assim como as
+      devidas aplicações da lei local, fornecer escoltas ou ajudar aqueles que
+      sofrerem assédio para se sintam seguros pelo o decorrer da conferência.
+      Nós valorizamos a sua participação. Esperamos que os participantes sigam
+      estas regras em salas de apresentação e workshops da conferência, além de
+      eventos sociais relacionados.
+    </p>
+    <h2>Sobre este código de conduta</h2>
+    <p>
+      Código de Conduta baseado em <a href="http://nodeconf.com/code-of-conduct.html">Node Conf's Code of Conduct</a>,
+      <a href="http://confcodeofconduct.com/index-pt-br.html">ConfCodeofConduct.com em Português Brasileiro</a>
+      e inspirado por <a href="http://2012.jsconf.us/#/about">http://2012.jsconf.us/#/about</a> &amp;
+      <a href="http://geekfeminism.wikia.com/wiki/Conference_anti-harassment/Policy">The Ada Initative</a>.
+    </p>
+    <p>Ajude este documento a melhorar em <a href="https://github.com/confcodeofconduct/confcodeofconduct.com/blob/master/index-pt-br.html">confcodeofconduct.com</a></p>
+    <p>Este trabalho está sob a licença <a href="http://creativecommons.org/licenses/by/3.0/deed.en_US">Creative Commons Attribution 3.0 Unported (CC BY 3.0)</a></p>
+  `
 }
