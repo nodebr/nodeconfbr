f204acbe4b0e7d93776e99972558167a38051f92
Angelo Rogério Rubin <angelorubin@gmail.com>
2016-04-28 16:02:20 -0300

    tradução para o inglês

--- components/locales/en/page.js
+++ components/locales/en/page.js
@@ -36,80 +36,54 @@ export default {
     </p>
     <p>
       <a href="http://www.eventick.com.br/nodeconfbr" target="_blank">
-        Compre seu ingresso
-      </a> e garanta uma vaga nesta NodeConf histórica!
+        Buy your ticket
+      </a> and ensure a place in this historic NodeConf!
     </p>`,
   code_of_conduct_description: `
-    <h1>Código de Conduta</h1>
+    <h1>Code of conduct</h1>
     <p>
-      Todos os participantes, palestrantes, patrocinadores e voluntários da
-      conferência precisam concordar com este código de conduta. Os
-      organizadores vão reforçar que este código seja seguido durante todo o
-      evento. Esperamos a cooperação de todos os participantes para ajudar a
-      garantir um ambiente seguro para todos.
+      All participants, speakers, sponsors and conference volunteers must agree to this code of conduct. 
+      The organizers will reinforce that this code is followed throughout the event. 
+      We expect the cooperation of all participants to help ensure a safe environment for all.
     </p>
-    <h2>Precisa de ajuda?</h2>
+    <h2>Need help?</h2>
     <p>
-      Fale com a NodeBR via
+      Talk to NodeBR:
         <a href="https://www.facebook.com/brNode">Facebook</a>,
         <a href="https://twitter.com/nodebr">Twitter</a>,
         <a href="mailto:nodebr@nodebr.org">e-mail</a>
-      ou envie uma conversa privada para meu Telegram
+      	or send a private conversation to my Telegram
         <a href="http://telegram.me/nodebr">@nodebr</a>.
-      Caso tenha algum problema sinta-se à vontade para falar, sua privacidade
-      será garantida.
+      	If you have any problems feel free to speak, your privacy will be guaranteed.
     </p>
-    <h2>Versão curta</h2>
+    <h2>Short version</h2>
     <p>
-      Nossa conferência é dedicada a fornecer uma experiência de conferência
-      livre de assédio para todos, independentemente do sexo, identidade de
-      gênero e expressão, idade, orientação sexual, deficiência, aparência
-      física, tamanho corporal, cor de pele, etnia, religião (ou falta dela) ou
-      escolhas de tecnologias. Nós não toleramos o assédio aos participantes da
-      conferência, sob qualquer forma. Linguagem e imagens sexuais não são
-      apropriadas em nenhum local da conferência, incluindo palestras,
-      workshops, festas, Twitter e outras mídias on-line. Os participantes que
-      violarem estas regras poderão ser punidos ou expulsos da conferência sem
-      restituição, a critério dos organizadores da conferência.
+      Our conference is dedicated to providing free conference experience harassment for all, regardless of gender, gender identity and expression, age, sexual orientation, disability, physical appearance, body size, skin color, ethnicity, religion (or lack thereof ) or choices technologies. We do not tolerate harassment to conference participants, in any form. Language and sexual images are not
+      appropriate in any location of the conference, including lectures,
+      workshops, parties, Twitter and other online media. Participants
+      violate these rules will be punished or expelled from the conference without
+      refund at the discretion of the conference organizers.
     </p>
-    <h2>Versão normal</h2>
+    <h2>Normal version</h2>
     <p>
-      Assédio inclui comentários verbais ofensivos relacionados ao gênero,
-      identidade de gênero e expressão, idade, orientação sexual, deficiência,
-      aparência física, tamanho corporal, cor de pele, etnia, religião, escolhas
-      de tecnologias, imagens sexuais em espaços públicos, intimidação
-      deliberada, perseguição, stalking, fotografias ou filmagens
-      constrangedoras, interrupção contínua das apresentações ou outros
-      eventos, contato físico inadequado e atenção sexual indesejada. Os
-      participantes que receberem uma solicitação para interromper qualquer
-      comportamento de assédio devem fazê-lo imediatamente. Os patrocinadores
-      também estão sujeitos às políticas antiassédio. Particularmente, os
-      patrocinadores não devem usar imagens, atividades ou outro material de
-      cunho sexual. As equipes de estandes e tendas (incluindo voluntários) não
-      devem utilizar roupas, uniformes ou trajes sexualizados ou de outra forma
-      criar um ambiente sexualizado. Se um participante se envolve em
-      comportamento de assédio, os organizadores da conferência podem tomar
-      todas as medidas que considerem adequadas, incluindo avisar o ofensor ou
-      expulsá-lo da conferência sem nenhum reembolso. Caso seja assediado,
-      perceba que alguém está sendo assediado ou tenha alguma dúvida, entre em
-      contato com um membro da equipe da conferência imediatamente. Membros da
-      equipe da conferência podem ser identificados pois estarão vestindo
-      camisetas marcadas. A equipe da conferência estará disposta a auxiliar os
-      participantes a contatarem a segurança do hotel ou local, assim como as
-      devidas aplicações da lei local, fornecer escoltas ou ajudar aqueles que
-      sofrerem assédio para se sintam seguros pelo o decorrer da conferência.
-      Nós valorizamos a sua participação. Esperamos que os participantes sigam
-      estas regras em salas de apresentação e workshops da conferência, além de
-      eventos sociais relacionados.
+      Harassment includes offensive verbal comments related to gender, gender identity and expression, age, sexual orientation, disability, physical appearance, body size, skin color, ethnicity, religion, technology choices, sexual images in public spaces, deliberate intimidation, persecution stalking, embarrassing photographs or filming, continuous interruption of presentations or other events, inappropriate physical contact and unwanted sexual attention. 
+      Participants receive a request to stop any harassing behavior should do so immediately. 
+      Sponsors are also subject to anti- harassment policies. Particularly, sponsors should not use images, activities or other sexually oriented material. 
+      The teams stands and tents (including volunteers) should not use clothes, uniforms or sexualized costumes or otherwise create a sexualized environment. 
+      If a participant engages in harassing behavior, conference organizers can take all the measures they consider appropriate, including advising the offender or drive it out of the conference with no refund. 
+      If harassed, realize that someone is being harassed or have any questions, please contact one conference staff member immediately. conference team members can be identified because they will be wearing shirts marked. 
+      The conference team will be willing to help participants contacting hotel security or local, as well as the proper application of local law, provide escorts or help those who suffer harassment so that they feel safe at during the conference. 
+      e value your participation. 
+      We expect participants to follow these rules in presentation rooms and conference workshops, and related social events.
     </p>
-    <h2>Sobre este código de conduta</h2>
+    <h2>About this code of conduct</h2>
     <p>
-      Código de Conduta baseado em <a href="http://nodeconf.com/code-of-conduct.html">Node Conf's Code of Conduct</a>,
-      <a href="http://confcodeofconduct.com/index-pt-br.html">ConfCodeofConduct.com em Português Brasileiro</a>
-      e inspirado por <a href="http://2012.jsconf.us/#/about">http://2012.jsconf.us/#/about</a> &amp;
+      Code of Conduct based on <a href="http://nodeconf.com/code-of-conduct.html">Node Conf's Code of Conduct</a>,
+      <a href="http://confcodeofconduct.com/index-pt-br.html">ConfCodeofConduct.com in Brazilian Portuguese</a>
+      and inspired by <a href="http://2012.jsconf.us/#/about">http://2012.jsconf.us/#/about</a> &amp;
       <a href="http://geekfeminism.wikia.com/wiki/Conference_anti-harassment/Policy">The Ada Initative</a>.
     </p>
-    <p>Ajude este documento a melhorar em <a href="https://github.com/confcodeofconduct/confcodeofconduct.com/blob/master/index-pt-br.html">confcodeofconduct.com</a></p>
-    <p>Este trabalho está sob a licença <a href="http://creativecommons.org/licenses/by/3.0/deed.en_US">Creative Commons Attribution 3.0 Unported (CC BY 3.0)</a></p>
+    <p>Support this document to improve in <a href="https://github.com/confcodeofconduct/confcodeofconduct.com/blob/master/index-pt-br.html">confcodeofconduct.com</a></p>
+    <p>This work is licensed under a <a href="http://creativecommons.org/licenses/by/3.0/deed.en_US">Creative Commons Attribution 3.0 Unported (CC BY 3.0)</a></p>
   `
 }
